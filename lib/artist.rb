c169e0067add338c357959c61df5dd3973614866
Meghan Flynn <mflynn333@gmail.com>
2020-01-24 18:14:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 
 class Artist
+  
+  extend
+  
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-  
-  extend memorable
 
   def initialize
     @@artists << self
